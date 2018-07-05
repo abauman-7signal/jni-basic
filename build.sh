0f13fd49fa3b30371ce348ce0bc2822d9b212471
Aaron Bauman <aaron.bauman@7signal.com>
2018-07-05 14:29:43 -0400

    Removes unused code (it was commented out)

--- build.sh
+++ build.sh
@@ -13,17 +13,6 @@ function buildJniLibrary() {
   javac HelloWorld.java
 }
 
-#function packageLibraryIntoJar() {
-#  cp build/lib/libhelloworld.jnilib build/com/sevensignal/helloworld
-#  jar -cf build/helloworld.jar -C build com/sevensignal/helloworld
-#}
-
-#function buildFunctionalTest() {
-#  javac -cp build/helloworld.jar:. -d build functional-test/java/com/sevensignal/mosfunctionaltester/TestJni.java
-#}
-
 createHeaderForNativeInterface
 buildNativeLibrary
 buildJniLibrary
-#packageLibraryIntoJar
-#buildFunctionalTest
