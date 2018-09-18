c1cfe5c831ebf50a83f9c35eacb497b9696d0d5e
Aaron Bauman <aaron.bauman@7signal.com>
2018-09-18 14:45:20 -0400

    provides debugging info during build and more closely aligns build settings to the dll-using-cygwin project

--- buildForWin.sh
+++ buildForWin.sh
@@ -1,17 +1,38 @@
 #!/bin/sh
 
+MODULE=HelloWorld
+
+function displayMessage() {
+  echo --------------------------------------------------
+  echo "  ${1}"
+  echo --------------------------------------------------
+}
+
+function processStatusOfLastCommand() {
+  if [ $? -ne 0 ] ; then
+    echo "F A I L E D"
+    exit 1
+  fi
+}
+
 function createHeaderForNativeInterface() {
-  javac -h . HelloWorld.java
+  displayMessage "C R E A T I N G    J N I    H E A D E R  =>  ${MODULE}"
+  javac -verbose -h . ${MODULE}.java
+  processStatusOfLastCommand
 }
 
 function buildNativeLibrary() {
-  gcc -v -D __int64="long long" -fPIC -Wall -Wl,--add-stdcall-alias -shared -lc -I. -I"${JAVA_HOME}\include" -I"${JAVA_HOME}\include\win32" -o HelloWorld.dll HelloWorld.cpp
+  displayMessage "B U I L D I N G    D L L  =>  ${MODULE}"
+  gcc -v -D__int64="long long" -Wall -Wl,--add-stdcall-alias -shared -I. -I"${JAVA_HOME}\include" -I"${JAVA_HOME}\include\win32" -o ${MODULE}.dll ${MODULE}.cpp
+  processStatusOfLastCommand
 }
 
 function buildJniLibrary() {
-  javac HelloWorld.java
+  displayMessage "B U I L D I N G    J A V A    A P P  =>  ${MODULE}"
+  javac -verbose ${MODULE}.java
+  processStatusOfLastCommand
 }
 
 createHeaderForNativeInterface
 buildNativeLibrary
-buildJniLibrary
+# buildJniLibrary
